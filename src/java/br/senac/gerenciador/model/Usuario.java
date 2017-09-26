cd170855bd7f9ab44b0bff1b259bba2923efad97
a915458 <a915458@02LAB000PC01.rn.senac.br>
2017-09-25 21:17:28 -0300

    Gerenciador Aula 29 09

---
+++ src/java/br/senac/gerenciador/model/Usuario.java
@@ -0,0 +1,70 @@
+package br.senac.gerenciador.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author a915458
+ */
+public class Usuario {
+    private String email;
+    private String senha;
+
+    public Usuario(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Usuario(String email) {
+        this.email = email;
+    }
+
+   
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.email);
+        hash = 29 * hash + Objects.hashCode(this.senha);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.senha, other.senha)) {
+            return false;
+        }
+        return true;
+    } 
+    
+}
---
+++ src/java/br/senac/rn/gerenciador/dao/UsuarioDAO.java
@@ -0,0 +1,28 @@
+package br.senac.rn.gerenciador.dao;
+
+import br.senac.gerenciador.model.Usuario;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author a915458
+ */
+public class UsuarioDAO {
+
+    private final static Map<String, Usuario> USUARIOS = new HashMap();
+    
+    static {
+        USUARIOS.put("eder@rn.senac.br", new Usuario("eder@rn.senac.br", "12345"));
+        USUARIOS.put("dario@rn.senac.br", new Usuario("dario@rn.senac.br", "987456"));
+        
+    }
+    public Usuario buscarPorEmailESenha(String email, String senha) {
+        if (!USUARIOS.containsKey(email)) {
+            return null;
+        }
+        
+    Usuario usuario = new Usuario(email, senha);
+    if (usuario.getSenha().equals(senha)) {
+        return usuario;
+    }
